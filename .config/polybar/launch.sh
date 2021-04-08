ac3214cde031cb2093b8980c8f110ef8a15c86fd
Louis-Philippe Bilodeau <78229833+Lpbill@users.noreply.github.com>
2021-04-08 00:22:12 -0400

    Used example for polybar's  launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,10 +3,5 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
-
-echo "Polybar launched..."
+echo "---" | tee -a /tmp/polybar1.log
+polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
